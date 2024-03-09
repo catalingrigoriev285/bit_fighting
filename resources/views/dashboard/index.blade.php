4a14c26f008584ecb2cc00a377a4b5998cbb34ba
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2024-03-09 14:17:17 +0200

    dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,21 @@
 @extends('dashboard.body')
 
 @section('content')
-    {{ auth()->user()->name}}
-    <a href="{{ route('auth.logout')}} ">logout</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Bit Fighting</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="{{route('dashboard.index')}}">Home</a>
+                    </li>
+                </ul>
+                <a href="{{route('auth.logout')}}" class="btn btn-primary">Log Out</a>
+            </div>
+        </div>
+    </nav>
 @endsection
